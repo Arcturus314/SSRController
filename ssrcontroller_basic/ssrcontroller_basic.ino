fd613e16e60350a157c43e09c3150cec555c383d
Kaveh Pezeshki <kaveh@pezeshki.org>
2021-03-28 17:30:24 -0700

    added pinout

---
+++ ssrcontroller_basic/ssrcontroller_basic.ino
@@ -0,0 +1,27 @@
+/*
+ * Kaveh Pezeshki
+ * MVP implementation of three-channel SSR controller
+ * kpezeshki@hmc.edu
+ * 03/28/2021
+*/
+
+#define ch0_pb  2
+#define ch0_out 3
+#define ch0_ain A3
+#define ch0_pot A0
+
+#define ch1_pb  4
+#define ch1_out 5
+#define ch1_ain A6
+#define ch1_pot A1
+
+#define ch2_pb  8
+#define ch2_out 9
+#define ch2_ain A7
+#define ch2_pot A2
+
+void setup() {
+}
+
+void loop() {
+}
